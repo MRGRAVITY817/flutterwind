24827f0b04361764a4efb9e38ae7fa7effde919c
Hoon Wee <mrgravity817@gmail.com>
2024-04-24 01:00:12 +0900

    feat(parse): parse padding for all sides

--- lib/src/parse_style.dart
+++ lib/src/parse_style.dart
@@ -24,7 +24,12 @@ Map<String, dynamic> parseStyle(String style) {
 }
 
 Map<String, double> parsePadding(List<String> classes) {
-  final paddingMap = <String, double>{};
+  final paddingMap = <String, double>{
+    "pt": 0,
+    "pr": 0,
+    "pb": 0,
+    "pl": 0,
+  };
 
   for (final className in classes) {
     if (className.startsWith("p-")) {
@@ -32,6 +37,18 @@ Map<String, double> parsePadding(List<String> classes) {
       paddingMap["pt"] = double.parse(padding);
       paddingMap["pr"] = double.parse(padding);
       paddingMap["pb"] = double.parse(padding);
+      paddingMap["pl"] = double.parse(padding);
+    } else if (className.startsWith("pt-")) {
+      final padding = className.split("-")[1];
+      paddingMap["pt"] = double.parse(padding);
+    } else if (className.startsWith("pr-")) {
+      final padding = className.split("-")[1];
+      paddingMap["pr"] = double.parse(padding);
+    } else if (className.startsWith("pb-")) {
+      final padding = className.split("-")[1];
+      paddingMap["pb"] = double.parse(padding);
+    } else if (className.startsWith("pl-")) {
+      final padding = className.split("-")[1];
       paddingMap["pl"] = double.parse(padding);
     }
   }
--- test/parse_padding_test.dart
+++ test/parse_padding_test.dart
@@ -2,16 +2,74 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterwind/src/parse_style.dart';
 
 void main() {
+  // system under test
+  final sut = parsePadding;
+
   group('parse padding', () {
-    test('p-4', () {
-      final style = "p-4";
+    test('padding every direction with same amount', () {
+      final input = ["p-4"];
 
-      final parsedStyle = parsePadding(style.split(" "));
+      final output = sut(input);
 
-      expect(parsedStyle['pt'], 4.0);
-      expect(parsedStyle['pr'], 4.0);
-      expect(parsedStyle['pb'], 4.0);
-      expect(parsedStyle['pl'], 4.0);
+      expect(output['pt'], 4.0);
+      expect(output['pr'], 4.0);
+      expect(output['pb'], 4.0);
+      expect(output['pl'], 4.0);
+    });
+
+    test('padding only left', () {
+      final input = ["pl-4"];
+
+      final output = sut(input);
+
+      expect(output['pt'], 0);
+      expect(output['pr'], 0);
+      expect(output['pb'], 0);
+      expect(output['pl'], 4.0);
+    });
+
+    test('padding only right', () {
+      final input = ["pr-4"];
+
+      final output = sut(input);
+
+      expect(output['pt'], 0);
+      expect(output['pr'], 4.0);
+      expect(output['pb'], 0);
+      expect(output['pl'], 0);
+    });
+
+    test('padding only top', () {
+      final input = ["pt-4"];
+
+      final output = sut(input);
+
+      expect(output['pt'], 4.0);
+      expect(output['pr'], 0);
+      expect(output['pb'], 0);
+      expect(output['pl'], 0);
+    });
+
+    test('padding only bottom', () {
+      final input = ["pb-4"];
+
+      final output = sut(input);
+
+      expect(output['pt'], 0);
+      expect(output['pr'], 0);
+      expect(output['pb'], 4.0);
+      expect(output['pl'], 0);
+    });
+
+    test('padding every direction with different amount', () {
+      final input = ["pt-4", "pr-8", "pb-12", "pl-16"];
+
+      final output = sut(input);
+
+      expect(output['pt'], 4.0);
+      expect(output['pr'], 8.0);
+      expect(output['pb'], 12.0);
+      expect(output['pl'], 16.0);
     });
   });
 }

2f32a6aeff647733a8c87bcb22f3717e00d022d6
Hoon Wee <mrgravity817@gmail.com>
2024-04-30 21:07:12 +0900

    feat: add default spacing scale

---
+++ lib/src/parse_style/defaults/default_spacing_scale.dart
@@ -0,0 +1,42 @@
+/// By default, Tailwind includes a generous and comprehensive numeric spacing scale.
+/// The values are proportional, so 16 is twice as much spacing as 8 for example.
+/// One spacing unit is equal to 0.25rem, which translates to 4px by default in common browsers.
+///
+/// More info: https://tailwindcss.com/docs/customizing-spacing#default-spacing-scale
+const Map<String, double> defaultSpacingScale = {
+  "0": 0,
+  "px": 1,
+  "0.5": 2,
+  "1": 4,
+  "1.5": 6,
+  "2": 8,
+  "2.5": 10,
+  "3": 12,
+  "3.5": 14,
+  "4": 16,
+  "5": 20,
+  "6": 24,
+  "7": 28,
+  "8": 32,
+  "9": 36,
+  "10": 40,
+  "11": 44,
+  "12": 48,
+  "14": 56,
+  "16": 64,
+  "20": 80,
+  "24": 96,
+  "28": 112,
+  "32": 128,
+  "36": 144,
+  "40": 160,
+  "44": 176,
+  "48": 192,
+  "52": 208,
+  "56": 224,
+  "60": 240,
+  "64": 256,
+  "72": 288,
+  "80": 320,
+  "96": 384,
+};
